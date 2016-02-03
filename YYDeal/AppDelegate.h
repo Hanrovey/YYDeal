a74572100078e5e403f6ba5cd6572f183601b528
chenxihang <chenxihang@ihefe.com>
2016-02-03 10:51:21 +0800

    first add all files

---
+++ YYDeal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYDeal
+//
+//  Created by ihefe-0004 on 16/2/3.
+//  Copyright © 2016年 Hanrovey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYDeal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYDeal
+//
+//  Created by ihefe-0004 on 16/2/3.
+//  Copyright © 2016年 Hanrovey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
